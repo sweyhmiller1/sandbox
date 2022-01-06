7490939f315c221b4149b977e9c6eda73a64876a
raphdias <82193521+raphdias@users.noreply.github.com>
2022-01-06 12:57:51 -0500

    text done
    jfslk

--- ezpd/lib/screens/text.dart
+++ ezpd/lib/screens/text.dart
@@ -1,113 +1,58 @@
-import '../flutter_flow/flutter_flow_theme.dart';
-import '../flutter_flow/flutter_flow_util.dart';
-import '../flutter_flow/flutter_flow_widgets.dart';
-import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+// Define a custom Form widget.
 
-class ListofoptionWidget extends StatefulWidget {
-  const ListofoptionWidget({Key key}) : super(key: key);
+final inputsArray = new List<String>();
+
+class MyCustomForm extends StatefulWidget {
+  const MyCustomForm({Key? key}) : super(key: key);
 
   @override
-  _ListofoptionWidgetState createState() => _ListofoptionWidgetState();
+  _MyCustomFormState createState() => _MyCustomFormState();
 }
 
-class _ListofoptionWidgetState extends State<ListofoptionWidget> {
-  TextEditingController textController;
-  final scaffoldKey = GlobalKey<ScaffoldState>();
+// Define a corresponding State class.
+// This class holds the data related to the Form.
+class _MyCustomFormState extends State<MyCustomForm> {
+  // Create a text controller and use it to retrieve the current value
+  // of the TextField.
+  final myController = TextEditingController();
 
   @override
-  void initState() {
-    super.initState();
-    textController = TextEditingController();
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    myController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: scaffoldKey,
-      appBar: AppBar(
-        backgroundColor: FlutterFlowTheme.primaryColor,
-        automaticallyImplyLeading: true,
-        actions: [],
-        centerTitle: true,
-        elevation: 4,
-      ),
-      backgroundColor: Color(0xFFF5F5F5),
-      body: SafeArea(
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            Column(
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                Container(
-                  width: MediaQuery.of(context).size.width * 0.5,
-                  height: MediaQuery.of(context).size.height * 0.5,
-                  decoration: BoxDecoration(
-                    color: Color(0xFFEEEEEE),
-                  ),
-                  child: TextFormField(
-                    controller: textController,
-                    obscureText: false,
-                    decoration: InputDecoration(
-                      hintText: '[Some hint text...]',
-                      hintStyle: FlutterFlowTheme.bodyText1,
-                      enabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Color(0x00000000),
-                          width: 1,
-                        ),
-                        borderRadius: const BorderRadius.only(
-                          topLeft: Radius.circular(4.0),
-                          topRight: Radius.circular(4.0),
-                        ),
-                      ),
-                      focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Color(0x00000000),
-                          width: 1,
-                        ),
-                        borderRadius: const BorderRadius.only(
-                          topLeft: Radius.circular(4.0),
-                          topRight: Radius.circular(4.0),
-                        ),
-                      ),
-                    ),
-                    style: FlutterFlowTheme.bodyText1,
-                  ),
-                ),
-                Container(
-                  width: 100,
-                  height: 100,
-                  decoration: BoxDecoration(
-                    color: Color(0xFFEEEEEE),
-                  ),
-                ),
-              ],
-            ),
-            FFButtonWidget(
-              onPressed: () {
-                print('Button pressed ...');
-              },
-              text: 'Button',
-              options: FFButtonOptions(
-                width: 130,
-                height: 40,
-                color: FlutterFlowTheme.primaryColor,
-                textStyle: FlutterFlowTheme.subtitle2.override(
-                  fontFamily: 'Poppins',
-                  color: Colors.white,
-                ),
-                borderSide: BorderSide(
-                  color: Colors.transparent,
-                  width: 1,
-                ),
-                borderRadius: 12,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+    // Fill this out in the next step.
   }
+}
+
+return TextField(
+controller: myController,
+);
+
+
+FloatingActionButton(
+// When the user presses the button, show an alert dialog containing
+// the text that the user has entered into the text field.
+onPressed: () {
+showDialog(
+context: context,
+builder: (context) {
+return AlertDialog(
+// Retrieve the text that the user has entered by using the
+// TextEditingController.
+content: Text(myController.text),
+);
+},
+);
+},
+tooltip: 'Show me the value!',
+child: const Icon(Icons.text_fields),
+),
+
+void main() {
+  inputsArray.add(myController.text);
 }
